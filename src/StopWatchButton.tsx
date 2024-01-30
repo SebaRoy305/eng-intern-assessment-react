341cb1611efa3bc19e4337f86b7aadecc7f96173
SebaRoy305 <roysebastienr@gmail.com>
2024-01-29 23:43:39 -0500

    The stopwatch is run on switch loops and boolean statements
    I am unsure of how to do this project properly and efficiently so I hope this is at least somewhat acceptable.

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-// Maximum number of laps that can be recorded
 const maxLaps = 25;
 
-// Define the props for the StopWatchButton component
 type StopWatchButtonProps = {
     type: 'start' | 'stop' | 'lap' | 'reset';
     onClick?: () => void;
@@ -13,7 +11,6 @@ type StopWatchButtonProps = {
 };
   
   export default function StopWatchButton({ type, onClick, timerOn, time, lapTimes }: StopWatchButtonProps) {
-    // Determine the button text based on the type and add corresponding tabIndex
     let buttonText, tabIndex;
     switch(type) {
         case 'start':
@@ -36,7 +33,6 @@ type StopWatchButtonProps = {
         buttonText = '';
         tabIndex = 0;
     }
-    // Determine whether the reset or lap buttons should be disabled
     const isLapDisabled = !timerOn || (lapTimes && lapTimes.length === 25);
     const isResetDisabled = time === 0;
     return(
@@ -44,10 +40,7 @@ type StopWatchButtonProps = {
             onClick={onClick} 
             aria-label={type}
             tabIndex={tabIndex}
-            // Disable the lap button when the timer is stopped or when the max number of lap times is reached. Disable reset button when the timer is already reset
             disabled={(type === 'lap' && isLapDisabled) || (type === 'reset' && isResetDisabled)}
-            >
-            {/* Display the button text, otherwise display the max laps reached message when max number is reached */}
             {lapTimes && lapTimes.length === maxLaps && timerOn && type === 'lap' ? "Maximum laps reached" : buttonText}
         </button>
     )
